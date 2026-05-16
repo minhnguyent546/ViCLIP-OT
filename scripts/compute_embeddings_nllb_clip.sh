10baa0d64b216f72fc37055b31bc5e188638f577
minhnguyent546 <minhnguyent546@gmail.com>
2026-05-16 14:11:50 +0700

    fix(scripts): correct language option for nllb

--- scripts/compute_embeddings_nllb_clip.sh
+++ scripts/compute_embeddings_nllb_clip.sh
@@ -2,7 +2,7 @@
 
 uv run python scripts/compute_embeddings_nllb_clip.py \
     --model nllb-clip-large-siglip:v1 \
-    --language vie_latn \
+    --language vie_Latn \
     --dataset_dir ./data/UIT-OpenViIC \
     --metadata_json_file test.json \
     --batch_size 64 \
