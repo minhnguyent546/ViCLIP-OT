99dbfacbcb141900d19d835495b2db56a55f1ebb
minhnguyent546 <minhnguyent546@gmail.com>
2026-05-16 09:15:48 +0700

    chore(scripts): add scripts/compute_embeddings_nllb_clip.sh

---
+++ scripts/compute_embeddings_nllb_clip.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+uv run python scripts/compute_embeddings_nllb_clip.py \
+    --model nllb-clip-large-siglip:v1 \
+    --language vie_latn \
+    --dataset_dir ./data/UIT-OpenViIC \
+    --metadata_json_file test.json \
+    --batch_size 64 \
+    --normalize
