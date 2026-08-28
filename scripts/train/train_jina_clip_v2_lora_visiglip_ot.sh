501beb3db53813cba87e0971ccce1979f976385a
minhnguyent546 <minhnguyent546@gmail.com>
2026-08-28 18:20:17 +0700

    feat(scripts): add training scripts for jina-clip-v2

---
+++ scripts/train/train_jina_clip_v2_lora_visiglip_ot.sh
@@ -0,0 +1,41 @@
+#!/usr/bin/env bash
+
+# One-time setup: UV_TORCH_BACKEND=cu128 uv sync --group jina-training
+uv run --no-sync python -m viclip_ot.train \
+  --optimizer adamw \
+  --adam_eps 1e-10 \
+  --seed 42 \
+  --model_config ./config/model.jina_clip_v2_lora_siglip.yaml \
+  --dataset_dir ./data/UIT-OpenViIC \
+  --train_batch_size 8 \
+  --gradient_accum_steps 16 \
+  --eval_batch_size 1 \
+  --train_crop_size 512 \
+  --eval_resize_size 512 \
+  --eval_crop_size 512 \
+  --checkpoints_dir ./checkpoints \
+  --num_epochs 15 \
+  --num_workers 4 \
+  --log_file_interval 3 \
+  --mixed_precision bf16 \
+  --lr 1e-4 \
+  --backbone_lr 1e-4 \
+  --weight_decay 1e-4 \
+  --scheduler one_cycle_lr \
+  --min_lr 1e-6 \
+  --lr_warmup_epochs 2 \
+  --lr_warmup_method linear \
+  --best_checkpoint_metrics t2i_R__1 \
+  --save_best_k 5 \
+  --save_best_k_only \
+  --max_grad_norm 1.0 \
+  --wandb_logging \
+  --wandb_project viclip_ot \
+  --wandb_name jina_clip_v2_lora_r16_a32_visiglip_ot \
+  --precomputed_image_embeddings_path ./data/UIT-OpenViIC-embeddings/train_image_embeddings_qwen3_vl_embedding_2b.pt \
+  --precomputed_caption_embeddings_path ./data/UIT-OpenViIC-embeddings/train_caption_embeddings_qwen3_vl_embedding_2b.pt \
+  --sim_graph_regularized_ot \
+  --sinkhorn_solver sinkhorn_unbalanced \
+  --criterion hybrid_sig_lip_tp_loss \
+  --sim_combine_method cross_modality \
+  --hybrid_sig_lip_tp_loss_sig_lip_loss_lambda 0.1
