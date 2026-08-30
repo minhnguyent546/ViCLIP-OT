5fc0416463eddb6fa05baf53bd91b2e69afd4d7f
minhnguyent546 <minhnguyent546@gmail.com>
2026-08-30 21:39:30 +0700

    feat(scripts): add trainng scripts for msiglip

---
+++ scripts/train/train_msiglip_224.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+uv run --no-sync python -m viclip_ot.train \
+    --optimizer adamw \
+    --adam_eps 1e-10 \
+    --seed 42 \
+    --model_config ./config/model.siglip_base_patch16_224_multilingual.yaml \
+    --dataset_dir ./data/UIT-OpenViIC \
+    --train_batch_size 8 \
+    --gradient_accum_steps 16 \
+    --eval_batch_size 32 \
+    --train_crop_size 224 \
+    --eval_resize_size 256 \
+    --eval_crop_size 224 \
+    --checkpoints_dir ./checkpoints \
+    --num_epochs 30 \
+    --num_workers 8 \
+    --log_file_interval 3 \
+    --mixed_precision bf16 \
+    --lr 2e-4 \
+    --backbone_lr 5e-5 \
+    --weight_decay 1e-4 \
+    --scheduler one_cycle_lr \
+    --min_lr 1e-6 \
+    --lr_warmup_epochs 2 \
+    --lr_warmup_method linear \
+    --best_checkpoint_metrics t2i_R__1 \
+    --save_best_k 5 \
+    --save_best_k_only \
+    --max_grad_norm 1.0 \
+    --wandb_logging \
+    --wandb_project viclip_ot \
+    --wandb_name msiglip_224 \
+    --criterion sig_lip_loss \
+    --eval_criterion sig_lip_loss
---
+++ scripts/train/train_msiglip_visiglip_ot_224.sh
@@ -0,0 +1,43 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+uv run --no-sync python -m viclip_ot.train \
+    --optimizer adamw \
+    --adam_eps 1e-10 \
+    --seed 42 \
+    --model_config ./config/model.siglip_base_patch16_224_multilingual.yaml \
+    --dataset_dir ./data/UIT-OpenViIC \
+    --train_batch_size 8 \
+    --gradient_accum_steps 16 \
+    --eval_batch_size 32 \
+    --train_crop_size 224 \
+    --eval_resize_size 256 \
+    --eval_crop_size 224 \
+    --checkpoints_dir ./checkpoints \
+    --num_epochs 30 \
+    --num_workers 8 \
+    --log_file_interval 3 \
+    --mixed_precision bf16 \
+    --lr 2e-4 \
+    --backbone_lr 5e-5 \
+    --weight_decay 1e-4 \
+    --scheduler one_cycle_lr \
+    --min_lr 1e-6 \
+    --lr_warmup_epochs 2 \
+    --lr_warmup_method linear \
+    --best_checkpoint_metrics t2i_R__1 \
+    --save_best_k 5 \
+    --save_best_k_only \
+    --max_grad_norm 1.0 \
+    --wandb_logging \
+    --wandb_project viclip_ot \
+    --wandb_name msiglip_sigrot_224 \
+    --precomputed_image_embeddings_path ./data/UIT-OpenViIC-embeddings/train_image_embeddings_qwen3_vl_embedding_2b.pt \
+    --precomputed_caption_embeddings_path ./data/UIT-OpenViIC-embeddings/train_caption_embeddings_qwen3_vl_embedding_2b.pt \
+    --sim_graph_regularized_ot \
+    --sinkhorn_solver sinkhorn_unbalanced \
+    --criterion hybrid_sig_lip_tp_loss \
+    --eval_criterion sig_lip_loss \
+    --sim_combine_method cross_modality \
+    --hybrid_sig_lip_tp_loss_sig_lip_loss_lambda 0.1
