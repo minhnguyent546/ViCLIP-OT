85021f680ee1458c39ab0ff7332b13ba10d1dc4f
minhnguyent546 <minhnguyent546@gmail.com>
2026-08-30 21:26:33 +0700

    feat(scripts): add inference script for msiglip with 224 image resolution

---
+++ scripts/inference/run_inference_msiglip_224.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+uv run --no-sync python -m viclip_ot.train \
+    --run_test_only \
+    --seed 42 \
+    --model_config ./config/model.msiglip_base_patch16_224_multilingual.yaml \
+    --dataset_dir ./data/UIT-OpenViIC \
+    --test_split_name test \
+    --train_crop_size 224 \
+    --eval_resize_size 256 \
+    --eval_crop_size 224 \
+    --eval_batch_size 32 \
+    --num_workers 4 \
+    --eval_criterion sig_lip_loss
